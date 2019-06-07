847a7c861252680b38d321b5546ee8ead162223b
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-06-07 10:15:26 -0400

    add semantic UI

--- problem-hunt-dashboard/frontend/src/components/problems/Form.js
+++ problem-hunt-dashboard/frontend/src/components/problems/Form.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {Button} from 'semantic-ui-react';
 
 export class Form extends Component {
   render () {
     return (
       <div>
         <h1>Add Problem Form</h1>
+        <Button primary>Submit Problem</Button>
       </div>
     );
   }
